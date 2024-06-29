ba9fb2b5cbf77461507cc24b218c44c423ac0cde
yaz008 <yaz008.yaz008@yandex.ru>
2024-06-29 10:55:02 +0300

    Refactor open

--- src/filemanip/open.hpp
+++ src/filemanip/open.hpp
@@ -7,13 +7,11 @@
 
 std::string open(const char* path) {
     std::ifstream file(path);
-    if (file.is_open()) {
-        std::stringstream buffer;
-        buffer << file.rdbuf();
-        return buffer.str();
-    }
-    else {
+    if (not file.is_open()) {
         std::string error = "Unable to open: ";
         throw std::runtime_error(error + path);
     }
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
 }
